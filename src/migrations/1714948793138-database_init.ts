bbf998af6cec803fef01819f7af6af9992e1c823
Afrejef69 <pxndxjeffrey.jr@gmail.com>
2024-05-26 18:29:23 -0600

    migrations

--- src/migrations/1714948793138-database_init.ts
+++ src/migrations/1714948793138-database_init.ts
@@ -5,13 +5,13 @@ export class DatabaseInit1714948793138 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "email" character varying(255) NOT NULL, "password" character varying(255) NOT NULL, "blogId" integer, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
       `CREATE TYPE "public"."blogs_genre_enum" AS ENUM('develop', 'design', 'lifestyle', 'technology', 'programming')`,
     );
     await queryRunner.query(
       `CREATE TABLE "blogs" ("id" SERIAL NOT NULL, "title" character varying(255) NOT NULL, "content" text NOT NULL, "genre" "public"."blogs_genre_enum" NOT NULL, CONSTRAINT "PK_e113335f11c926da929a625f118" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "email" character varying(255) NOT NULL, "password" character varying(255) NOT NULL, "blogId" integer, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "users" ADD CONSTRAINT "FK_76934127c9bd6759d0673346347" FOREIGN KEY ("blogId") REFERENCES "blogs"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
